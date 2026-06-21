c88b5da7bfd4b14c510a4550fe59ecc49dcab63c
ramibn20041998-create <ramibn20041998@gmail.com>
2026-06-21 02:52:23 +0200

    Delete pi-digital-city directory

--- pi-digital-city/backend/src/utils/logger.js
+++
@@ -1,8 +0,0 @@
-/* Minimal structured logger - swap for pino/winston in a larger deployment */
-const ts = () => new Date().toISOString();
-
-module.exports = {
-  info: (...args) => console.log(`[INFO ${ts()}]`, ...args),
-  warn: (...args) => console.warn(`[WARN ${ts()}]`, ...args),
-  error: (...args) => console.error(`[ERROR ${ts()}]`, ...args),
-};
